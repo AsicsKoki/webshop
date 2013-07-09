5e658d14f5e14ef4b6d1d1bb44faa644b1270d65
AsicsKoki <cpt.koki@gmail.com>
2013-07-09 11:05:04 +0200

    - Initial commit

---
+++ js/javaShop.js
@@ -0,0 +1,22 @@
+(function(){
+
+
+	
+	var buyBtn = document.getElementById("buyBtn");
+	var buyInput = document.getElementById("buyInput");
+	var	alert = document.getElementById("alert");
+
+	buyBtn.onclick = function(){
+		if (buyInput.value > 10){
+			$(document.getElementById("alert")).modal();
+		} else{
+			buyBtn.innerHTML = "purchased";
+			document.getElementById("buyBtn").classList.add("btn-danger");
+			this.disabled = true;
+		}
+	};
+
+
+
+
+}())

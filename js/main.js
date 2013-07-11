c7b240642f2447dfe4d4f98afb16bab7be0ebc2d
AsicsKoki <cpt.koki@gmail.com>
2013-07-11 16:41:59 +0200

    Refectored the code

--- js/main.js
+++ js/main.js
@@ -2,21 +2,54 @@
 
 
 	
-	var buyBtn = document.getElementById("buyBtn");
-	var buyInput = document.getElementById("buyInput");
-	var	alert = document.getElementById("alert");
+	var buyBtn = $("#buyBtn");
+	var buyInput = $("#buyInput");
+	var	alert = $("#alert");
 
-	buyBtn.onclick = function(){
-		if (buyInput.value > 10 || buyInput.value == 0){
-			$(document.getElementById("alert")).modal();
+	buyBtn.click(function(){
+		if (buyInput.val() > 10 || buyInput.val() == 0){
+			$("#alert").modal();
 		} else{
-			buyBtn.innerHTML = "purchased";
-			document.getElementById("buyBtn").classList.add("btn-danger");
-			this.disabled = true;
+			$(this).text("Purchased");
+			$(this).addClass("btn-danger");
+			$(this).attr("disabled", "disabled");
 		}
-	};
+	});
 
 
 
 
 }())
+
+jQuery(document).ready(function($) {
+		$("div.columnLeft > footer").prepend("<button id=\"readMoreBtn\">Read more</button>");
+
+		$("div.columnLeft > footer > button#readMoreBtn").on('click', function(){
+			var btn = $(this);
+
+			if (btn.text() == "Read more") {
+
+				$("div.more").slideDown()
+				btn.text("Read less");
+			} else{
+				$("div.more").slideUp();
+				btn.text("Read more");
+			}
+		});
+			$("button#next").on('click', function(){
+				var img = $("div.columnRight > img:visible");
+				img.hide();
+				var imgNext = img.next();
+				if (!imgNext.length) imgNext =  $("div.columnRight > img").first();
+				imgNext.show();
+
+			})
+			$("button#previous").on('click', function(){
+				var img = $("div.columnRight > img:visible");
+				img.hide();
+				var imgPrevious = img.prev();
+				if (!imgPrevious.length) imgPrevious =  $("div.columnRight > img").last();
+				imgPrevious.show();
+			});
+});
+

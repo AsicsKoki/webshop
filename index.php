2cca4fa28da0ab5e09dd5ad3f5154143bbe80f32
AsicsKoki <cpt.koki@gmail.com>
2013-07-29 16:23:25 +0200

    added php elements and dynamic data creation to prooducts table and product page.

---
+++ index.php
@@ -0,0 +1,74 @@
+<?php 
+	
+	// $con = mysqli_connect("localhost","root","","webshop"); 
+	// $result = mysqli_query($con,"SELECT * FROM table products");
+	
+	// var_dump($result);
+
+$conn = mysql_connect("localhost","root","","webshop");
+if(! $conn )
+{
+  die('Could not connect: ' . mysql_error());
+}
+$sql = 'SELECT * FROM products';
+
+mysql_select_db('webshop');
+$retval = mysql_query( $sql, $conn );
+if(! $retval )
+{
+  die('Could not get data: ' . mysql_error());
+}
+
+ ?>
+
+<!doctype html>
+<html>
+<head>
+	<link rel="stylesheet" href="css/styles.css">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/bootstrap-responsive.css">
+	<link rel="stylesheet" href="css/bootstrap-responsive.min.css">
+</head>
+<body id="background">
+	<div id="mainElement">
+		<header id="header">Konstantin's web shop</header>
+		<div id="elementOne">
+			<div class="side" >BANNER</div>
+			<div id="central">
+				<table class="table table-hover">
+					<thead>
+						<th>Product name</th>
+						<th>Color</th>
+						<th>Price</th>
+						<th>Action</th>
+						<th>Quantity</th>
+					</thead>
+					<tbody>
+						<?php 
+							while($row = mysql_fetch_assoc($retval)) {
+
+						 ?>
+						<tr>
+							<td class="column1"><a href="product.php?id=<?php echo $row["id"]; ?>" target="_blank"><?php echo $row["name"]; ?></a></td>
+							<td class="column2"><?php echo $row["color"]; ?></td>
+							<td class="column3"><i class="icon-tag"></i><?php echo $row["price"]; ?></td>
+							<td class="column4"><a class="btn btn-info" target="_blank" href="product.php?id=<?php echo $row["id"]; ?>"><i class="icon-info-sign"></i>More info</a></td>
+							<td class="column5"><?php echo $row["price"] ?></td>
+						</tr>
+						<?php 
+							}
+						 ?>
+					</tbody>
+				</table>
+			</div>
+			<div class="side">BANNER</div>
+		</div>
+		<footer id="footer">(2013) All rights reserved</footer>
+	</div>
+	<script src="bootstrap.js"></script>
+	<script src="bootstrap.min.js"></script>
+	<script src="main.js"></script>
+</body>
+</html>
+<?php mysql_close($conn); ?>
---
+++ product.php
@@ -0,0 +1,66 @@
+<?php 
+	$conn = mysql_connect("localhost","root","","webshop");
+	//$query = "SELECT * FROM products where id= '1'";
+	//var_dump($query);
+		if(! $conn )
+	{
+	  	die('Could not connect: ' . mysql_error());
+	}
+	$id = $_GET["id"];
+	$sql = "SELECT * FROM products where id= '$id'";
+
+	mysql_select_db('webshop');
+	$retval = mysql_query( $sql, $conn );
+		if(! $retval )
+	{
+		die('Could not get data: ' . mysql_error());
+	}
+	$arr = [];
+
+		while($row = mysql_fetch_assoc($retval))
+	$arr[] = $row;
+	//var_dump($arr);
+	$arr[0]["name"];
+ ?>
+<!doctype html>
+<html>
+<head>
+	<link rel="stylesheet" href="css/styles.css">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/bootstrap-responsive.css">
+	<link rel="stylesheet" href="css/bootstrap-responsive.min.css">
+</head>
+<body id="background">
+	<div id="mainElement">
+		<header id="header">Konstantin's web shop</header>
+		<div id="elementOne">
+			<div class="side" >BANNER</div>
+			<div id="central">
+				<div class="columnLeft">
+					<?php echo $arr[0]["description"]; ?>
+				<footer><button class="btn btn-primary" type="button" style="float: left">Purchase item</button><input style="float: left; width: 120px;" type="text" size="2" placeholder="Enter quantity here!">
+				</footer></div>
+				<div class="columnRight"><img class="img-polaroid" src="images/<?php echo $arr[0]['image']; ?>"/></div>
+			</div>
+			<div class="side">BANNER</div>
+		</div>
+		<footer id="footer">(2013) All rights reserved</footer>
+		    <div id="alert" class="modal hide fade">
+	    <div  class="modal-header">
+		    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+		    <h3>Alert!</h3>
+	    </div>
+	    <div class="modal-body">
+	  	  <p>Nema na lageru</p>
+	    </div>
+	    <div class="modal-footer">
+	   	 <a href="#" class="btn" data-dismiss="modal">Close</a>
+	    </div>
+    </div>
+    <script src="js/jquery-1.10.2.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/javaShop.js"></script>
+	</div>
+</body>
+</html>

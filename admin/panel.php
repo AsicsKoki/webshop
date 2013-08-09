78d2fa18d978b69a1d0cf4fdd05079422e5373bd
Konstantin Velickovic <cpt.koki@gmail.com>
2013-08-09 09:08:43 +0200

    added a dropdown

--- admin/panel.php
+++ admin/panel.php
@@ -1,3 +1,46 @@
 <?php 
 	echo "NICE, YOU ARE HERE!";
  ?>
+
+ <!doctype HTML>
+ <html>
+<head>
+	<link rel="stylesheet" href="../css/styles.css">
+	<link rel="stylesheet" href="../css/bootstrap.css">
+	<link rel="stylesheet" href="../css/bootstrap.min.css">
+	<link rel="stylesheet" href="../css/bootstrap-responsive.css">
+	<link rel="stylesheet" href="../css/bootstrap-responsive.min.css">
+</head>
+<body id="background">
+<div id="mainElement">
+	<header id="header">Konstantin's web shop</header>
+	<div width="100%">CONTROL PANEL</div>
+		<div>
+			<ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
+
+ 			<li class="dropdown-submenu">
+    		<a tabindex="-1" href="#">More options</a>
+    		<ul class="dropdown-menu">
+    </ul>
+  </li>
+</ul>
+		 </div>
+		<div id="elementOne">
+			<div id="central">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</div>
+				<div class="side">BANNER</div>
+			</div>
+		<footer id="footer">(2013) All rights reserved</footer>
+	</div>
+	<script src="/..js/bootstrap.js"></script>
+	<script src="/..js/bootstrap.min.js"></script>
+	<script src="/..js/main.js"></script>
+</body>
+</html>
+<?php mysql_close($conn); ?>

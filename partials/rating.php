718b0e3b006e56378b05fb508ab11e6df40da814
Koki <cpt.koki@gmail.com>
2013-09-17 15:17:12 +0200

    minor edit

--- partials/rating.php
+++ partials/rating.php
@@ -7,3 +7,5 @@
 	<div class="total_votes">vote data</div>
 </div>
 <!-- http://net.tutsplus.com/tutorials/html-css-techniques/building-a-5-star-rating-system-with-jquery-ajax-and-php/ -->
+
+<!-- render rating -funkcija, prosledjujemo calculate rating funkciju -->
--- product.php
+++ product.php
@@ -254,16 +254,16 @@
 	$(window).load(function() {
 		$('.flexslider').flexslider();
 	});
-$('.ratings_stars').hover(
-	// Handles the mouseover
-	function() {
-		$(this).prevAll().andSelf().addClass('ratings_over');
-		$(this).nextAll().removeClass('ratings_vote');
-		},
-	// Handles the mouseout
-	function() {
-		$(this).prevAll().andSelf().removeClass('ratings_over');
-		set_votes($(this).parent());
+	$('.ratings_stars').hover(
+		// Handles the mouseover
+		function() {
+			$(this).prevAll().andSelf().addClass('ratings_over');
+			$(this).nextAll().removeClass('ratings_vote');
+			},
+		// Handles the mouseout
+		function() {
+			$(this).prevAll().andSelf().removeClass('ratings_over');
+			set_votes($(this).parent());
 		}
 	);
 	</script>

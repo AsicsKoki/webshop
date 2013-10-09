46c7c8e09727c46012f4995159ba84a29916ec8a
misaizdaleka <milos.boskovic@gmail.com>
2013-10-09 14:59:14 +0200

    oop practise

--- OOP.php
+++ OOP.php
@@ -1,3 +1,19 @@
-<?php 
-	echo "123";
+<!doctype html>
+<html>
+<head>
+	<title>OOP ucenje</title>
+	<?php include "oop_lib.php"; ?>
+</head>
+<body>
+<?php
+	$koki = new person();
+	$mika = new person();
+	$koki->set_name("Konstantin Velickovc");
+	$mika->set_name("Miljana Brankovic");
+
+	echo $koki->get_name();
+	echo $mika->get_name();
+
  ?>
+</body>
+</html>
